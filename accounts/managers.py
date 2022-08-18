da3af453d4646590ac318831315995ee6fb3c52d
Elyas Sekhavati Nia <e.b35927@gmail.com>
2022-08-18 16:15:27 +0430

    add User model to accounts app

---
+++ accounts/managers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, first_name, last_name, email, phone_number, password):
+        user = self.model(first_name=first_name, last_name=last_name,
+                          email=email, phone_number=phone_number)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, phone_number, password):
+        user = self.create_user(first_name, last_name, email, phone_number, password)
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=50, null=True, blank=True, verbose_name='نام')
+    last_name = models.CharField(max_length=50, null=True, blank=True, verbose_name='نام خانوادگی')
+    email = models.EmailField(max_length=100, unique=True, null=True, blank=True, verbose_name='ایمیل')
+    phone_number = models.CharField(max_length=11, unique=True, verbose_name='شماره تلفن')
+    is_active = models.BooleanField(default=True, verbose_name='وضعیت فعال بودن')
+    is_admin = models.BooleanField(default=False, verbose_name='وضعیت ادمین بودن')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    updated = models.DateTimeField(auto_now=True, verbose_name='تاریخ به‌روزرسانی')
+
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    def __str__(self):
+        return f'{self.email} - {self.phone_number}'
+
+    @property
+    def is_staff(self):
+        return self.is_admin
